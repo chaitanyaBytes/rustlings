093bbff1e2b3ab5bf2344f9811ad01005c16e2a0
Chaitanya <chaitanyagupta172004@gmail.com>
2025-08-11 15:53:04 +0000

    complete 18

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -10,6 +10,9 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+
+    let nums = 1..=num;
+    nums.product()
 }
 
 fn main() {
